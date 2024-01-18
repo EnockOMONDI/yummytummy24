b9fd4b029a56c41595faba893ee3bc801f0da3f3
enockomondi <djseanizellkenya@gmail.com>
2024-01-18 04:40:15 +0300

    working app

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ echo "Building the project JDA..."
 
 pip install -r requirements.txt
 
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+# python3 manage.py makemigrations --noinput
+# python3 manage.py migrate --noinput
  
